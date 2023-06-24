c01a3cd71e7c8ac2b173a4ec2569e4893362e254
Vladislav Lanskikh <vladislav_lanskikh@epam.com>
2023-06-24 19:16:03 +0200

    Add auth0, add ability to create links

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,4 +1,3 @@
-// /graphql/types/Link.ts
 import { builder } from "../builder";
 
 builder.prismaObject("Link", {
@@ -19,5 +18,39 @@ builder.queryField("links", (t) =>
     cursor: "id",
     resolve: (query, _parent, _args, _ctx, _info) =>
       prisma.link.findMany({ ...query }),
+  })
+);
+
+builder.mutationField("createLink", (t) =>
+  t.prismaField({
+    type: "Link",
+    args: {
+      title: t.arg.string({ required: true }),
+      description: t.arg.string({ required: true }),
+      url: t.arg.string({ required: true }),
+      imageUrl: t.arg.string({ required: true }),
+      category: t.arg.string({ required: true }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      const { title, description, url, imageUrl, category } = args;
+
+      if (!(await ctx).user) {
+        throw new Error("You have to be logged in to perform this action");
+      }
+
+      // To fix ids not incrementing
+      // await prisma.$executeRaw`SELECT setval(pg_get_serial_sequence('"Link"', 'id'), coalesce(max(id)+1, 1), false) FROM "Link";`
+
+      return prisma.link.create({
+        ...query,
+        data: {
+          title,
+          description,
+          url,
+          imageUrl,
+          category,
+        },
+      });
+    },
   })
 );
